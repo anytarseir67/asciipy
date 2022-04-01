034988183d30064c6edce4cdfc145fe5244a5134
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2022-04-01 01:35:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import pathlib
 HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text().replace('`pip install asciipython`', '')
+README = (HERE / "README.md").read_text().replace('`pip install asciipy-any`', '')
 
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
